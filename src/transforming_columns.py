44d8e8dd48c1dbaf2441a7b05a3bea98404e4dc9
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2025-02-20 19:34:36 +0100

    Add transforming_columns.py

---
+++ src/transforming_columns.py
@@ -0,0 +1,49 @@
+# In Python, define a schema
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+# Programmatic way to define a schema
+fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
+    StructField('UnitID', StringType(), True),
+    StructField('IncidentNumber', IntegerType(), True),
+    StructField('CallType', StringType(), True),
+    StructField('CallDate', StringType(), True),
+    StructField('WatchDate', StringType(), True),
+    StructField('CallFinalDisposition', StringType(), True),
+    StructField('AvailableDtTm', StringType(), True),
+    StructField('Address', StringType(), True),
+    StructField('City', StringType(), True),
+    StructField('Zipcode', IntegerType(), True),
+    StructField('Battalion', StringType(), True),
+    StructField('StationArea', StringType(), True),
+    StructField('Box', StringType(), True),
+    StructField('OriginalPriority', StringType(), True),
+    StructField('Priority', StringType(), True),
+    StructField('FinalPriority', IntegerType(), True),
+    StructField('ALSUnit', BooleanType(), True),
+    StructField('CallTypeGroup', StringType(), True),
+    StructField('NumAlarms', IntegerType(), True),
+    StructField('UnitType', StringType(), True),
+    StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+    StructField('FirePreventionDistrict', StringType(), True),
+    StructField('SupervisorDistrict', StringType(), True),
+    StructField('Neighborhood', StringType(), True),
+    StructField('Location', StringType(), True),
+    StructField('RowID', StringType(), True),
+    StructField('Delay', FloatType(), True)])
+# Use the DataFrameReader interface to read a CSV file
+sf_fire_file = "/opt/spark/src/data/sf-fire-calls.csv"
+fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
+# Convert columns to timestamp and drop string columns
+fire_ts_df = (fire_df
+    .withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))
+    .drop("CallDate")
+    .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))
+    .drop("WatchDate")
+    .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"),
+    "MM/dd/yyyy hh:mm:ss a"))
+    .drop("AvailableDtTm"))
+
+# Select the converted columns
+(fire_ts_df
+    .select("IncidentDate", "OnWatchDate", "AvailableDtTS")
+    .show(5, False))
